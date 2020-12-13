6a0b11e59758f5be09b63947dc9b3e9e0b87969c
Prokopiv <halyna.kotlyar@gmail.com>
2020-12-14 00:11:55 +0200

    menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import './styles.css';
+import './main.css';
+import menu from './menu.json';
+import cardsTmpl from './cards.hbs';
+import './switch-toggle.js';
+
+const menuRef = document.querySelector('.js-menu');
+const cardsMarkUp = cardsTmpl(menu);
+
+menuRef.insertAdjacentHTML('afterbegin', cardsMarkUp);
+
+// function createMenuCardsMarkup(menu) {
+//     // return menu.map(cardsTmpl), join('');
+//     return cardsTmpl(menu);;
+
+// }
+
